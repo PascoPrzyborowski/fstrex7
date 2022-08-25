d93540c111c94e56c62d38b4e587170d07abbe42
PascoPrzyborowski <unity01@gmx.net>
2022-08-25 14:07:44 +0200

    fstrex7

---
+++ fstrex7.py
@@ -0,0 +1,16 @@
+
+
+#Task 1
+
+
+textorg = "The Quick BroWnFoX jumps over the laZy Dog!"
+textupp = textorg.upper()
+textdow1 = textorg.lower()
+textdow2 = textdow1[0:18]
+print()
+print("Orginal - String: " + textorg)
+print()
+print("String in uppercase: " + textupp)
+print()
+print("String in lowercase: " + textdow2 + "!")
+print()
